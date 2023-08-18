aea459e63be4891064698813f45e1f19d4542a6c
Lime <alsayyd.medo72@gmail.com>
2023-08-18 19:11:10 +0300

    fix error

--- src/app/components/Widgets/Widgets.jsx
+++ src/app/components/Widgets/Widgets.jsx
@@ -33,7 +33,7 @@ const Widgets = () => {
   return (
     <div className="xl:w-[600px] hidden lg:inline ml-8">
       <div className="w-[90%] xl:w-[100%] sticky top-0 bg-white py-1.5 z-50">
-        <div className="flex items-center p-3 rounded-full bg-red-300 relative ">
+        <div className="flex items-center p-3 rounded-full  relative ">
           <SearchIcon className="h-5 z-50 text-gray-500" />
           <input
             type="text"
